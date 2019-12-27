f70d23a84f17d8ffb17ec8f1b72975ab327945ca
risonakamo <hmykanae@gmail.com>
2019-12-27 12:36:41 -0500

    organised into functions

--- scshuffle.js
+++ scshuffle.js
@@ -1,45 +1,69 @@
 async function scshuffle()
 {
-    //this button toggles the queue
-    var toggleQueueButton=document.querySelector(".playbackSoundBadge__showQueue");
+    await toggleQueue();
 
-    //open the queue
-    toggleQueueButton.click();
+    await duplicateCurrent();
 
-    //if the queue is not yet open because the previous click closed it, open it again
-    if (!document.querySelector(".playbackSoundBadge").classList.contains("m-queueVisible"))
-    {
-        toggleQueueButton.click();
-    }
+    await scrollTrackList();
 
-    //open the 3 dot menu of the current song in the tracklist
-    document.querySelector(".m-active .queueItemView__more").click();
-    //click the add to next up, duplicating the current song
-    document.querySelector(".addToNextUp").click();
+    await toggleShuffle();
 
-    var tracklist=document.querySelector(".queue__scrollableInner");
+    await toggleQueue();
 
-    //scroll to bottom of tracklist and click autoplay button
-    await scrollTrackList(tracklist);
-
-    //click shuffle button
-    var shufflebutton=document.querySelector(".shuffleControl");
-    shufflebutton.click();
-
-    //if shuffle button was already clicked and is now off, click it again
-    if (!shufflebutton.classList.contains("m-shuffling"))
-    {
-        shufflebutton.click();
-    }
-
-    //close the queue and open it again
-    toggleQueueButton.click();
-    toggleQueueButton.click();
+    // //close the queue and open it again
+    // toggleQueueButton.click();
+    // toggleQueueButton.click();
     console.log("done");
 }
 
-function scrollTrackList(tracklist)
+// open the queue. if the queue is already open, will open it again. includes a wait time
+// to ensure the queue is open
+function toggleQueue()
 {
+    console.log("toggling queue");
+    return new Promise((resolve)=>{
+        //this button toggles the queue
+        var toggleQueueButton=document.querySelector(".playbackSoundBadge__showQueue");
+
+        //open the queue
+        toggleQueueButton.click();
+
+        //if the queue is not yet open because the previous click closed it, open it again
+        if (!document.querySelector(".playbackSoundBadge").classList.contains("m-queueVisible"))
+        {
+            setTimeout(()=>{
+                toggleQueueButton.click();
+            },200);
+        }
+
+        setTimeout(()=>{
+            resolve();
+        },500);
+    });
+}
+
+// the queue must be open and the current song must be in view.
+// duplicates the currently selected song.
+function duplicateCurrent()
+{
+    return new Promise((resolve)=>{
+        //open the 3 dot menu of the current song in the tracklist
+        document.querySelector(".m-active .queueItemView__more").click();
+        //click the add to next up, duplicating the current song
+        document.querySelector(".addToNextUp").click();
+
+        setTimeout(()=>{
+            resolve();
+        },200);
+    });
+}
+
+// the track list must be open and in view. attempts to scroll to the bottom
+// of the track list and click the autoplay button
+function scrollTrackList()
+{
+    var tracklist=document.querySelector(".queue__scrollableInner");
+
     tracklist.scrollTop=tracklist.scrollHeight;
 
     return new Promise((resolve)=>{
@@ -62,5 +86,24 @@ function scrollTrackList(tracklist)
                 scrollTrackList(tracklist);
             }
         },300);
+    });
+}
+
+// shuffle button must be in view. engages shuffle, flipping it if it was
+// already on.
+function toggleShuffle()
+{
+    return new Promise((resolve)=>{
+        var shufflebutton=document.querySelector(".shuffleControl");
+        shufflebutton.click();
+
+        if (!shufflebutton.classList.contains("m-shuffling"))
+        {
+            shufflebutton.click();
+        }
+
+        setTimeout(()=>{
+            resolve();
+        },500);
     });
 }
