43f54dbc995d2825bcc802d524d3d54a482525d5
risonakamo <hmykanae@gmail.com>
2019-12-31 14:07:27 -0500

    setup script execution

--- popup.js
+++ popup.js
@@ -2,5 +2,17 @@ window.onload=main;
 
 function main()
 {
+    chrome.tabs.query({
+        active:true,
+        currentWindow:true,
+        url:"https://soundcloud.com/*"
+    },(tabs)=>{
+        if (!tabs.length)
+        {
+            return;
+        }
 
+        chrome.tabs.executeScript(tabs[0].id,{file:"test.js"});
+        window.close();
+    });
 }
